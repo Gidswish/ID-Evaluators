00f9020b2f333f959df60f447071457672bb59e4
Swish <m1pro@Swish.local>
2025-12-17 12:39:16 +0000

    Fix Next Image for Supabase storage

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -13,4 +13,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
